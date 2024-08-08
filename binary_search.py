56e9909b34cf8ec3449d1d74d4646093476c6043
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-08 20:34:57 +0530

    linear search implementation in Python for finding an element's index in a list

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(list,target):
+    first=0
+    last=len(list)-1
+
+    while first<=last:
+        midpoint=(first+last)//2
+
+        if midpoint==target:
+            return midpoint
+        elif midpoint<target:
+            first=midpoint+1
+        else:
+            last=midpoint-1
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target is found in list",index)
+    else:
+        print("Target is not found in list")
+
+number=[1,2,3,4,5,6,7,8,9,10]
+result=binary_search(number,11)
+verify(result)
