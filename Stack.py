974285dad69a77d5c04c6157900a2c604115e2c8
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-31 13:54:05 +0530

    Added Reverse function

--- Stack.py
+++ Stack.py
@@ -1,13 +1,12 @@
 class Stack:
     def __init__(self):
-        self.item=[]
+        self.item = []
 
     def is_empty(self):
-        return len(self.item)==0
+        return len(self.item) == 0
 
-    def push(self,data):
+    def push(self, data):
         self.item.append(data)
-
 
     def pop(self):
         if not self.is_empty():
@@ -15,18 +14,21 @@ class Stack:
         else:
             raise IndexError("Stack is empty")
 
-    def peek(self):
-        if not self.is_empty():
-            return self.item[-1]
-        else :
-            raise IndexError("Stack is empty")
-    def size(self):
-        return len(self.item)
-        
-s1=Stack()
-s1.push(10)
-s1.push(20)
-s1.push(30)
-print("top element",s1.peek())
-print("remove element",s1.pop())
-print("top element",s1.peek())
+    def reverse(self):
+        temp_stack = Stack()
+        while not self.is_empty():
+            temp_stack.push(self.pop())
+        self.item = temp_stack.item
+
+
+s1 = Stack()
+s1.push(110)
+s1.push(204)
+s1.push(300)
+# s1.pop()
+print("Original stack:", s1.item)
+
+
+s1.reverse()
+print("Reversed stack:", s1.item)
+
