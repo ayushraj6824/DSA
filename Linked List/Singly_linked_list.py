4b6dffdfd992d15807c69cb2e649d90ff69de47f
Ayush kumar <ayush.17862@stu.upes.ac.in>
2025-06-07 21:33:45 +0530

    Add insert_at_start and insert_at_end methods to singly linked list implementation

--- Linked List/Singly_linked_list.py
+++ Linked List/Singly_linked_list.py
@@ -9,4 +9,16 @@ class SLL:
         self.start=start
     def is_empty(self):
         return self.start==None
+    def insert_at_start(self,item):
+        new_node=Node(item,self.start)
+        self.start=new_node 
+    def insert_at_end(self,item):
+        new_node=Node(item)
+        if self.is_empty():
+            self.start=new_node
+            return
+        current=self.start
+        while current.next:
+            current=current.next
+        current.next=new_node   
     
