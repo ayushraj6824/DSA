7f8dd3d5a1eaeff1a0a0b8ddd742d1dd7147bdbc
Ayush kumar <ayush.17862@stu.upes.ac.in>
2025-07-09 18:58:40 +0530

    Add initial implementations of factorial, Fibonacci, and sum functions

---
+++ Recursion/Factorial.py
@@ -0,0 +1,12 @@
+# Recursion is a technique where a function calls itself.
+# It's useful for solving problem that can be broken down into smaller ,similar sub- problems. 
+
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    return n*factorial(n-1)
+
+
+
+print(factorial(5))
---
+++ Recursion/find_sum.py
@@ -0,0 +1,8 @@
+def find_sum(n):
+    if n==1:
+        return 1
+    return n + find_sum(n-1)
+
+# Example
+print(find_sum(9))
+
