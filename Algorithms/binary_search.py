4cb1cd9bc9a1eec6cef3fd081f19d43b1319ed52
Ayush kumar <ayush.17862@stu.upes.ac.in>
2025-08-13 19:12:04 +0530

    Update binary_search.py example usage and remove unused cases; add twosum function implementation

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -16,16 +16,9 @@ def binary_search(arr, target):
 
 
 # Example usage:
-arr = [1, 2, 3, 4, 5]       
+arr = [ 3 ]       
 target = 3
 result = binary_search(arr, target) 
 print("Element found at index:", result if result != -1 else "Not Found")
 
-# Example usage with a target not in the array
-target_not_found = 6    
-result_not_found = binary_search(arr, target_not_found)
-print("Element found at index:", result_not_found if result_not_found != -1 else "Not Found")
-# Example usage with an empty array
-empty_arr = []  
-result_empty = binary_search(empty_arr, target)
-print("Element found at index:", result_empty if result_empty != -1 else "Not Found")   
+ 
