886aed1ae859816966cfc9dd0c934a40e402ec5b
Ayush kumar <ayush.17862@stu.upes.ac.in>
2025-08-17 02:13:16 +0530

    Refactor mergeTwoLists function to remove class encapsulation and improve readability

--- Algorithms/merge_two_list.py
+++ Algorithms/merge_two_list.py
@@ -1,17 +1,31 @@
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
 
-        if list1.val <= list2.val:
-            list1.next=self. mergeTwoLists(list1.next,list2)
-            return list1
-        else:
-            list2.next=self. mergeTwoLists(list1,list2.next)
-            return list2
-        
+    if list1.val <= list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    
 
+# Example usage
+# Create two sorted linked lists
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
 
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
 
+# Merge the two lists
+merged_list = mergeTwoLists(list1, list2)
+
+# Print the merged list
+while merged_list:
+    print(merged_list.val, end=" -> ")
+    merged_list = merged_list.next
